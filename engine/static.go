3cd50860b8add5eebdb7abbbd4beeb8000b897d6
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2021-02-03 23:46:24 +0300

    Add log formatting

--- engine/static.go
+++ engine/static.go
@@ -1,24 +1,26 @@
 package engine
 
-// Prints a message to the log from the current component.
-func LogInfo(msg string) {
-	instance.logger.Info(instance.currentComponent, msg)
+import "fmt"
+
+// Prints a message to the log from the current component, formatting the msg using fmt.Sprintf.
+func LogInfo(msg string, values ...interface{}) {
+	instance.logger.Info(instance.currentComponent, fmt.Sprintf(msg, values...))
 }
 
-// Prints a warning to the log from the current component.
-func LogWarning(msg string) {
-	instance.logger.Warning(instance.currentComponent, msg)
+// Prints a warning to the log from the current component, formatting the msg using fmt.Sprintf.
+func LogWarning(msg string, values ...interface{}) {
+	instance.logger.Warning(instance.currentComponent, fmt.Sprintf(msg, values...))
 }
 
-// Prints an error to the log from the current component.
-func LogError(msg string) {
-	instance.logger.Error(instance.currentComponent, msg)
+// Prints an error to the log from the current component, formatting the msg using fmt.Sprintf.
+func LogError(msg string, values ...interface{}) {
+	instance.logger.Error(instance.currentComponent, fmt.Sprintf(msg, values...))
 }
 
 // Same as LogInfo, but prints only if app is in debug mode.
-func LogDebug(msg string) {
+func LogDebug(msg string, values ...interface{}) {
 	if instance.currentApp == nil || instance.currentApp.Debug {
-		LogInfo(msg)
+		LogInfo(msg, values...)
 	}
 }
 
@@ -37,7 +39,21 @@ func LoadPrefab(resId int) (*SceneObject, error) {
 	return instance.LoadPrefab(resId)
 }
 
-// Shortcut for (engine *AmphionEngine) RunTask(task Task)
+// Shortcut for (engine *AmphionEngine) RunTask(task Task).
 func RunTask(task Task) {
 	instance.RunTask(task)
+}
+
+// Shortcut for (engine *AmphionEngine) BindEventHandler(code int, handler EventHandler).
+// Binds an event handler for the specified event code.
+// The handler will be invoked in the event loop goroutine, when the event with the specified code is raised.
+func BindEventHandler(eventCode int, handler EventHandler) {
+	instance.BindEventHandler(eventCode, handler)
+}
+
+// Shortcut for (engine *AmphionEngine) UnbindEventHandler(code int, handler EventHandler).
+// Unbinds the event handler for the specified event code.
+// The handler will no longer be invoked, when the event with the specified code is raised.
+func UnbindEventHandler(eventCode int, handler EventHandler) {
+	instance.UnbindEventHandler(eventCode, handler)
 }
